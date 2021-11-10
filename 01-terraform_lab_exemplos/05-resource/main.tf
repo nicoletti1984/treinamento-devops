08fe8977ef8fdd6ab441b7fe6cb35b44593cb732
Diego Nicoletti <diegonicoletti@uol.com.br>
2021-11-10 21:28:24 +0000

    2021-11-10 - Aula Terraform

--- 01-terraform_lab_exemplos/05-resource/main.tf
+++ 01-terraform_lab_exemplos/05-resource/main.tf
@@ -73,15 +73,23 @@ provider "aws" {
   region = "sa-east-1"
 }
 resource "aws_instance" "web" {
-  subnet_id     = "subnet-05d2f48a5e97f0b1a"
-  ami= "ami-054a31f1b3bf90920"
+  subnet_id     = "subnet-0e3bf872589dc8206"
+  ami= "ami-0e66f5495b4efdd0f"
   instance_type = "t2.micro"
+  key_name = "treinamento-itau-diego"
+  associate_public_ip_address = true
   root_block_device {
     encrypted = true
     volume_size = 8
   }
   tags = {
-    Name = "ec2-zerati-tf"
+    Name = "ec2-diego-tf1"
   }
+}
+
+# https://www.terraform.io/docs/language/values/outputs.html
+output "instance_public_dns" {
+  value = [aws_instance.web.public_dns, aws_instance.web.public_ip, aws_instance.web.private_ip]
+  description = "Mostra o DNS e os IPs publicos e privados da maquina criada."
 }
 # /////
