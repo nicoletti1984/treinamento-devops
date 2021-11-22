7e608c56a26b83d018a073feb94b3ab4ccbee26e
Diego Nicoletti <diegonicoletti@uol.com.br>
2021-11-22 17:46:29 +0000

    chave

--- 01-terraform_lab_exemplos/00-exercicios/01-criar-maquina-com-variavel/main.tf
+++ 01-terraform_lab_exemplos/00-exercicios/01-criar-maquina-com-variavel/main.tf
@@ -59,8 +59,3 @@ resource "aws_instance" "web" {
     Name = "ec2-diego-tf-html"
   }
 }
-
-resource "aws_key_pair" "chave_key" {
-  key_name   = "chave_key-diego"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCsc6YnccoGFh56GwYj5HjbSCttOzgj8z9uoCLdu3m+hamVH4dyExFKbCTnG6o+b4am7H+9yk0JEt4HlWwzN3MfaLgRoXHVRH3SpY2LzxfzOBl/xaGgQLRjx0gK7VIFhvfbhAOKtRNST2tb2QwphcEpjU2L98OiFTi0fdnGHNVmBZO35wiuRjbq2VAusqT35Z8GEkxbWHEI3hrT143voU+b8kbJycT/uuJU00q2hdXx4uaUPvkqUEro6HX0EAV08R0oodNoj4+J3KPMaJVspKnLuS1SXuGp3VpOSHK3DNoXciK6FJoCRqdbHylugC9FB+f5ytQSe3m+VKRo3BusmZxEzDg8HbuhNMLTns7qe+FI7nj+cl6tOvo85vf7mRKY3GHJdNL8J31PV/w+tZTE9qu7r1zGBK9bkcWbzQaBQs3vFkHr5dUfuHt/C8TrqFj+vRC2zamnblbBHmK1UN2svv8x5wt4XtIh79LvCVb+eveo1gsLH5I2IvZSMoCUA96N688= ubuntu@ec2-turma2-diego-dev"
-}
