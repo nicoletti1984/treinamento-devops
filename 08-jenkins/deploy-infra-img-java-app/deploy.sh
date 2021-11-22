db69c6fb16ea22ae67dcd625938ff94ba8c12798
Diego Nicoletti <diegonicoletti@uol.com.br>
2021-11-22 15:50:12 +0000

    chave

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -21,4 +21,4 @@ sleep 30 # 10 segundos
 cd ../ansible
 
 echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa ]"
-ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/id_rsa
+ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key home/ubuntu/.ssh/id_rsa
