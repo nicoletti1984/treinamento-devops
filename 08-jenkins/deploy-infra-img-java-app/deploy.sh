6c2e0758958c1b4c5f0e680d41d25df20f54a08b
Diego Nicoletti <diegonicoletti@uol.com.br>
2021-11-22 17:31:18 +0000

    caminho

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -10,8 +10,9 @@ terraform apply -auto-approve
 echo "Aguardando criação de maquinas ..."
 sleep 30 # 10 segundos
 
-echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo
+#echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo
 #echo "$(/home/ubuntu/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+echo $"[jenkins]" > ../ansible/hosts # cria arquivo
 echo "$(terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
 
 
