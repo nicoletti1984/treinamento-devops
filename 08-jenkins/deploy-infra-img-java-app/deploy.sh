18f5e37efa94957d23dc019014531476a1a64db7
Diego Nicoletti <diegonicoletti@uol.com.br>
2021-11-22 15:39:22 +0000

    Ajuste caminho terraform

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -11,7 +11,9 @@ echo "Aguardando criação de maquinas ..."
 sleep 30 # 10 segundos
 
 echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo
-echo "$(/home/ubuntu/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+#echo "$(/home/ubuntu/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+
 
 echo "Aguardando criação de maquinas ..."
 sleep 30 # 10 segundos
