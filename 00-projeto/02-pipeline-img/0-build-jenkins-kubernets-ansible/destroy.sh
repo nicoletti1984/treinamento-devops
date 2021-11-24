5848f41cf56de42b044bb370a4f868ce823fc524
Diego Nicoletti <diegonicoletti@uol.com.br>
2021-11-24 13:05:15 +0000

    ls

--- 00-projeto/02-pipeline-img/0-build-jenkins-kubernets-ansible/destroy.sh
+++ 00-projeto/02-pipeline-img/0-build-jenkins-kubernets-ansible/destroy.sh
@@ -4,8 +4,8 @@ cd 00-projeto/02-pipeline-img/0-build-jenkins-kubernets-ansible/0-terraform
 ID_MAQUINA=$(terraform output | grep id | awk '{print $2;exit}')
 echo ${ID_MAQUINA/\",/}
 
-cd ../2-terraform-ami/
-TF_VAR_resource_id=xxx TF_VAR_versao=v0.1.1 terraform destroy -auto-approve
+# cd ../2-terraform-ami/
+# TF_VAR_resource_id=xxx TF_VAR_versao=v0.1.1 terraform destroy -auto-approve
 
 cd ../0-terraform
 terraform destroy -auto-approve
