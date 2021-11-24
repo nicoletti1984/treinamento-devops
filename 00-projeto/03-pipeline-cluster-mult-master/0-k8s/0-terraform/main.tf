0f86d9410aa6e0a167f67ccc1011a327daa49729
Diego Nicoletti <diegonicoletti@uol.com.br>
2021-11-24 17:37:06 +0000

    Ajuste Grupo

--- 00-projeto/03-pipeline-cluster-mult-master/0-k8s/0-terraform/main.tf
+++ 00-projeto/03-pipeline-cluster-mult-master/0-k8s/0-terraform/main.tf
@@ -8,7 +8,7 @@ data "http" "myip" {
 
 resource "aws_instance" "k8s_proxy" {
   ami           = "ami-0e66f5495b4efdd0f"
-  subnet_id     = "subnet-0e3bf872589dc8206"
+  subnet_id     = "subnet-092b12a9fa786bdef"
   instance_type = "t2.medium"
   key_name = "chave_key-diego"
   associate_public_ip_address = true
@@ -25,7 +25,7 @@ resource "aws_instance" "k8s_proxy" {
 resource "aws_instance" "k8s_masters" {
 #  ami           = "ami-0e66f5495b4efdd0f"
   ami           = "ami-04c8d39612f50448b"
-  subnet_id     = "subnet-0e3bf872589dc8206"  
+  subnet_id     = "subnet-0a00cda81bbe15079"  
   instance_type = "t2.large"
   key_name = "chave_key-diego"
   associate_public_ip_address = true
@@ -64,7 +64,7 @@ resource "aws_instance" "k8s_workers" {
 resource "aws_security_group" "acessos_masters" {
   name        = "k8s-acessos_masters"
   description = "acessos inbound traffic"
-  vpc_id = "vpc-0aba9c677ce2a010c"
+  vpc_id = "vpc-0b7bc0aae8788da62"
 
   ingress = [
     {
@@ -89,20 +89,20 @@ resource "aws_security_group" "acessos_masters" {
       self             = true
       to_port          = 0
     },
-    {
-      cidr_blocks      = []
-      description      = "Libera acesso k8s_haproxy"
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = [
-        "sg-0de56713847c43dbf",
-        //"${aws_security_group.acessos_haproxy.id}",
-      ]
-      self             = false
-      to_port          = 0
-    },
+    # {
+    #   cidr_blocks      = []
+    #   description      = "Libera acesso k8s_haproxy"
+    #   from_port        = 0
+    #   ipv6_cidr_blocks = []
+    #   prefix_list_ids  = []
+    #   protocol         = "-1"
+    #   security_groups  = [
+    #     "sg-0de56713847c43dbf",
+    #     //"${aws_security_group.acessos_haproxy.id}",
+    #   ]
+    #   self             = false
+    #   to_port          = 0
+    # },
     {
       cidr_blocks      = [
         "0.0.0.0/0",
@@ -140,7 +140,7 @@ resource "aws_security_group" "acessos_masters" {
 resource "aws_security_group" "acessos_haproxy" {
   name        = "k8s-haproxy"
   description = "acessos inbound traffic"
-  vpc_id = "vpc-0aba9c677ce2a010c"
+  vpc_id = "vpc-0b7bc0aae8788da62"
 
   ingress = [
     {
@@ -154,34 +154,34 @@ resource "aws_security_group" "acessos_haproxy" {
       security_groups: null,
       self: null
     },
-    {
-      cidr_blocks      = []
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = [
-        "sg-066c221458cc25c63",
-        #aws_security_group.acessos_masters.id,
-      ]
-      self             = false
-      to_port          = 0
-    }, 
-    {
-      cidr_blocks      = []
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = [
-        #aws_security_group.acessos_workers.id,
-        "sg-086a3e76368b941e6",
-      ]
-      self             = false
-      to_port          = 0
-    },
+    # {
+    #   cidr_blocks      = []
+    #   description      = ""
+    #   from_port        = 0
+    #   ipv6_cidr_blocks = []
+    #   prefix_list_ids  = []
+    #   protocol         = "-1"
+    #   security_groups  = [
+    #     "sg-066c221458cc25c63",
+    #     #aws_security_group.acessos_masters.id,
+    #   ]
+    #   self             = false
+    #   to_port          = 0
+    # }, 
+    # {
+    #   cidr_blocks      = []
+    #   description      = ""
+    #   from_port        = 0
+    #   ipv6_cidr_blocks = []
+    #   prefix_list_ids  = []
+    #   protocol         = "-1"
+    #   security_groups  = [
+    #     #aws_security_group.acessos_workers.id,
+    #     "sg-086a3e76368b941e6",
+    #   ]
+    #   self             = false
+    #   to_port          = 0
+    # },
     {
       cidr_blocks      = []
       description      = ""
@@ -217,7 +217,7 @@ resource "aws_security_group" "acessos_haproxy" {
 resource "aws_security_group" "acessos_workers" {
   name        = "k8s-workers"
   description = "acessos inbound traffic"
-  vpc_id = "vpc-0aba9c677ce2a010c"
+  vpc_id = "vpc-0b7bc0aae8788da62"
 
   ingress = [
     {
